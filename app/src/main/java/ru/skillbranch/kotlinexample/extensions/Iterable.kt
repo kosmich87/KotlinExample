3bb86a555cae70a78c3680862c4c177f5c8e9953
kkurtukov <konstantinkg@gmail.com>
2021-03-25 19:15:31 +0600

    -hometask_2 drop last until

--- app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
@@ -8,7 +8,10 @@ fun String.email() = this.trim().toLowerCase()
 
 fun String.isValidEmail() = Patterns.EMAIL_ADDRESS.matcher(this).matches()
 
-fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): () -> List<T> = {
-    this.dropLastWhile(predicate)
-    this.dropLast(1)
+fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T> {
+    if (!isEmpty()) {
+        var lastItem = this.last(predicate)
+        return this.dropLast(size - this.indexOf(lastItem))
+    }
+    return emptyList()
 }
